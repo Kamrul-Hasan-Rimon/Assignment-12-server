8481322ba61ec48a80f87675135532bfe65f9de4
kamrul hasan <lnahar958@gmail.com>
2025-05-31 13:19:19 +0600

    server code

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     app.post("/jwt", async (req, res) => {
       let user = req.body;
@@ -953,7 +953,7 @@ async function run() {
       }
     });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
